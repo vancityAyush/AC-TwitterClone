e63cd25dfc1c6ce9443e7649c6795f3d224967c9
Ayush Kumar <eliasakeleven@gmail.com>
2020-12-29 03:17:27 +0530

    Tabs Implemented and Send Tweet feature done!

--- app/src/main/java/com/ak11/ac_twitterclone/TweetsTab.java
+++ app/src/main/java/com/ak11/ac_twitterclone/TweetsTab.java
@@ -8,9 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
 public class TweetsTab extends Fragment {
     private ListView listView;
+    final ArrayList<HashMap<String,String>> tweetList = new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,6 +28,19 @@ public class TweetsTab extends Fragment {
         View view= inflater.inflate(R.layout.fragment_tweets_tab, container, false);
 
         listView = view.findViewById(R.id.tweetsListView);
+
+        final SimpleAdapter simpleAdapter = new SimpleAdapter(getContext(),tweetList,
+                android.R.layout.simple_list_item_2, new String[]{"tweetUsername","tweetValue"},
+                new int[]{android.R.id.text1, android.R.id.text2});
+        try{
+            ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Tweet");
+
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
 
         return view;
